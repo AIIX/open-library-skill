c01596ccb0f2641122bfd3376a2c7b458303abb7
Aditya Mehra <Aix.m@outlook.com>
2018-04-10 20:14:38 +0930

    Add Skill Open-Library

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# open-library-skill
-Search for ebooks by title/author name mycroft skill
+## Open-Library-skill
+Search for ebooks by title/author name using OpenLibrary API
+
+## Description 
+#### Installation of skill:
+* Download or Clone Git (run: git clone https://github.com/AIIX/open-library-skill inside /opt/mycroft/skills)
+* Create /opt/mycroft/skills folder if it does not exist
+* Extract Downloaded Skill into a folder. "open-library-skill". (Clone does not require this step)
+* Copy the open-library-skill folder to /opt/mycroft/skills/ folder
+
+## Examples
+
+###### Search for book by title
+* "Hey Mycroft, find book by title story of my life"
+
+## Credits 
+(AIX) Aditya Mehra
---
+++ __init__.py
@@ -0,0 +1,71 @@
+import re
+import json
+import requests
+import openlibrary
+from adapt.intent import IntentBuilder
+from os.path import join, dirname
+from string import Template
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.skills.context import *
+from mycroft.util import read_stripped_lines
+from mycroft.util.log import getLogger
+from mycroft.messagebus.message import Message
+
+__author__ = 'aix'
+
+LOGGER = getLogger(__name__)
+
+class OpenLibrarySkill(MycroftSkill):
+    def __init__(self):
+        super(OpenLibrarySkill, self).__init__(name="OpenLibrarySkill")
+
+    @intent_handler(IntentBuilder("SearchByTitle").require("FindByTitleKeyword").build())
+    def handle_search_by_title_intent(self, message):
+        utterance = message.data.get('utterance').lower()
+        utterance = utterance.replace(message.data.get('FindByTitleKeyword'), '')
+        searchString = utterance.encode('utf-8')
+        api = openlibrary.BookSearch()
+        res = api.get_by_title(searchString)
+        bookisbn = res.docs[0].isbn[0]        
+        method = "GET"
+        read_url = "http://openlibrary.org/api/volumes/brief/isbn/"
+        read_data = "{0}.json".format(bookisbn)
+        response = requests.request(method,read_url+read_data)
+        try:
+            book = json.loads(response.text)
+            bookstatus = book["items"][0]["status"];
+            bookurl = book["items"][0]["itemURL"];
+            bookcover = book["items"][0]["cover"]["large"];
+            booktitle = res.docs[0].title
+            bookauthor = res.docs[0].author
+            bookpublisher = res.docs[0].publisher[0]
+            bookyear = res.docs[0].first_publish_year
+            bookobject = {};
+            bookobject['bkstatus'] = bookstatus
+            bookobject['bkurl'] = bookurl 
+            bookobject['bkcover'] = bookcover
+            bookobject['bkauthor'] = bookauthor 
+            bookobject['bkpublisher'] = bookpublisher
+            bookobject['bkyear'] = bookyear 
+            bookobject['bktitle'] = booktitle
+            bookObjJson = json.dumps(bookobject, ensure_ascii=False)
+            resultSpeak = "Found book {0} written by author {1}, first published by {2} in the year {3}".format(booktitle, bookauthor, bookpublisher, bookyear)
+            self.speak(resultSpeak)
+            downloadstatus = self.checkavailable(bookstatus)
+            self.enclosure.ws.emit(Message("bookObject", {'desktop': {'data': bookObjJson}}))
+    
+        except:
+            self.speak("No Books Found")
+            
+    def checkavailable(self, status):
+        if status == "'full access":
+            #self.speak("Would you like to read this book ?", expect_response=True)
+            return "downloadable"
+        else:
+            return "unavailable"
+            
+    def stop(self):
+        pass
+    
+def create_skill():
+    return OpenLibrarySkill()
